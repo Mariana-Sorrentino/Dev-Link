bd386bdc27104b8d68e740cfa19eb553e2345098
Mariana-Sorrentino <106826264+Mariana-Sorrentino@users.noreply.github.com>
2022-11-22 13:07:14 -0300

    Update firebaseConnection.js

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtgD6Wq6TqaeeCinSbjaYwfT6s-ZJD7RM",
-  authDomain: "devlink-5cbac.firebaseapp.com",
-  projectId: "devlink-5cbac",
-  storageBucket: "devlink-5cbac.appspot.com",
-  messagingSenderId: "516241998309",
-  appId: "1:516241998309:web:e3d586dc9e375433c92f0c",
-  measurementId: "G-N5GW2JZB60"
+  apiKey: "SuaAPIkey",
+  authDomain: "AUTHDOMAINLINK",
+  projectId: "IDPROJECT",
+  storageBucket: "SEUBUCKET",
+  messagingSenderId: "SENDERID",
+  appId: "IDGERAL",
+  measurementId: "ID"
 };
 
 const firebaseApp = initializeApp(firebaseConfig)
